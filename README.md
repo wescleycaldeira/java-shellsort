cb1730627f6ad05c1c1ab98da30971accf9b9ecf
Wescley <wescley13sri@gmail.com>
2021-04-14 08:06:20 -0300

    Shell Sort Algorithm

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# java-shellsort
-Shellsort Algorithm
+# Shell Sort
+Shell Sort Algorithm
+
+- Variation of insertion sort
+- Goal is to reduce the amount of shifting 
+- Knuth Sequence: (3k - 1) / 2. K is based the lenght of array
+- In-place
+- Worst Case: O(n²) - Quadratic
+- Unstable
---
+++ src/com/wcaldeira/Main.java
@@ -0,0 +1,29 @@
+package com.wcaldeira;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2){
+            System.out.println("GAP ::: " + gap);
+            for(int i = gap; i < intArray.length; i++){
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement){
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+
+                intArray[j] = newElement;
+            }
+        }
+
+        for (int element : intArray){
+            System.out.println(element);
+        }
+    }
+
+}
